a7643766cc55225169065548f37f8816d3f4b1cf
aman-senpai <jemscollin1@gmail.com>
2023-10-04 20:24:01 +0530

    Create custom error class

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connnection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
@@ -9,23 +7,8 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
-    if (err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map((error) => {
-            if (error.type === 'field') {
-                return { message: error.msg, field: error.path };
-            }
-        });
-        return res.status(400).send({ errors: formattedErrors });
-    }
-
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send({
-            errors: [
-                {
-                    message: err.reason
-                }
-            ]
-        })
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
 
     res.status(400).send({
