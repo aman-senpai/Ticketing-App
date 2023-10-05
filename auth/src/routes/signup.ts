8c4b09957923c5792e38f626f42639f23ed706f1
aman-senpai <jemscollin1@gmail.com>
2023-10-05 15:07:03 +0530

    Create new custom error that prompts existing email user, implement password hasing for securely saving the password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connnection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -20,10 +21,23 @@ router.post("/api/users/signup", [
         throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
 
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        // console.log("Email in use.");
+        // return res.send({});
+        throw new BadRequestError("Email in use.")
+    }
+
+    const user = User.build({
+        email, password
+    })
+
+    await user.save();
+
+    res.status(201).send(user);
 })
 
 export { router as signupRouter };
